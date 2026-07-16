fcf5c157c576a36f00e69a1012f7d5a1c970c1aa
Renan <renanlink44@gmail.com>
2026-07-15 23:06:45 -0300

    correcao no ligth-them (duplicado) // add funcao ao botao e definida sua classe

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+const switcher = document.querySelector(".btn");
+
+switcher.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  var className = document.body.className;
+  if (className == "light-theme") {
+    this.textContent = "Dark mode";
+  } else {
+    this.textContent = "Light mode";
+  }
+
+  console.log("current class name:" + className);
+});
